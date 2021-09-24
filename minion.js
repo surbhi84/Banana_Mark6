e58d9640ea6c65689708d829e3fca72cc7e47895
surbhi84 <56334321+surbhi84@users.noreply.github.com>
2021-09-25 03:47:37 +0530

    feat: clickHandler() defined

--- minion.js
+++ minion.js
@@ -1,3 +1,17 @@
 var translateBtn = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#text-input");
 var textOutput = document.querySelector("#text-output");
+
+translateBtn.addEventListener("click", clickHandler);
+
+function clickHandler() {
+  var inputText = textInput.value;
+
+  fetch(constructUrl(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedTxt = json.contents.translated;
+      textOutput.innerText = translatedTxt;
+    });
+  // .catch(errorHandler());
+}
